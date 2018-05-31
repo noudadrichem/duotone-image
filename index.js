a790cd254843e3dcc7648e47e8004217bf02baaf
info@noudadrichem.com <info@noudadrichem.com>
2018-05-31 17:52:01 +0200

    init

---
+++ index.js
@@ -0,0 +1,79 @@
+function getImageWidthAndHeight(imageUrl) {
+  return new Promise(resolve => {
+    const IMG = new Image();
+
+    IMG.src = imageUrl;
+    IMG.onload = function() {
+
+      const imageSize = {
+        width: this.width,
+        height: this.height
+      }
+
+      resolve(imageSize)
+    }
+  });
+}
+
+function convertToDueToneToMatrixString(color1, color2) {
+  let value = [];
+  value = value.concat(
+    [color1[0] / 256 - color2[0] / 256, 0, 0, 0, color2[0] / 256]);
+
+  value = value.concat(
+    [color1[1] / 256 - color2[1] / 256, 0, 0, 0, color2[1] / 256]);
+
+  value = value.concat(
+    [color1[2] / 256 - color2[2] / 256, 0, 0, 0, color2[2] / 256]);
+
+  value = value.concat([0, 0, 0, 1, 0]);
+
+  return value.join(' ')
+}
+
+function makeDuoToneImage({ domNode, color1, color2, imageWidth, imageHeight, imageUrl }) {
+  const toInsertPhotoTo = document.querySelector(domNode)
+  const duoToneString = convertToDueToneToMatrixString(color1, color2)
+  const rarararaaaandommmmm = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 8);
+
+  getImageWidthAndHeight(imageUrl)
+    .then(imageSize => {
+      const { width: widthFromImgNode, height: heightFromImgNode } = imageSize
+
+      let width = imageWidth !== undefined ? imageWidth : widthFromImgNode;
+      let height = imageHeight !== undefined ? imageHeight : heightFromImgNode;
+
+      toInsertPhotoTo.innerHTML = `
+        <svg width="${width}" height="${height}" viewBox="0 0 ${width} ${height}" id="duotone" preserveAspectRatio="xMidYMid slice">
+          <defs>
+            <filter id="${rarararaaaandommmmm}">
+              <feColorMatrix type="matrix" values="${duoToneString}"/>
+            </filter>
+          </defs>
+          <image filter="url(#${rarararaaaandommmmm})" xlink:href="${imageUrl}"/>
+        </svg> 
+      `
+
+      const slidingTagLiAfterStyle = document.createElement("style");
+      slidingTagLiAfterStyle.innerHTML = `
+        ${domNode} {
+          overflow: hidden;
+          line-height: 0;
+        }
+
+        ${domNode} svg {
+          color-interpolation-filters: sRGB;
+          width: 100%;
+          height: 100%;
+        }
+
+        ${domNode} svg image {
+          width: 100%;
+        }
+      `
+
+      document.head.appendChild(slidingTagLiAfterStyle)
+    })
+}
+
+export default makeDuoToneImage
---
+++ readme.md
@@ -0,0 +1,33 @@
+# Create duotone images
+
+> This is a first attempt, I really appriciate feedback!
+
+### Preview
+![screen shot 2018-05-31 at 17 46 11](https://user-images.githubusercontent.com/16288476/40792791-15351b50-64fb-11e8-95a7-29677ea862f1.png)
+
+### Install:
+
+```bash
+npm install duotone-image --save
+```
+
+### How to use
+```javascript
+import makeDuoToneImage from 'duotone-image'
+
+makeDuoToneImage({
+  domNode: '.image',
+  imageUrl: 'https://images.unsplash.com/photo-1505881502353-a1986add3762',
+  color1: [24, 192, 102],
+  color2: [0, 0, 30]
+})
+```
+
+### Options
+
+| options (key) | type         | required  |
+| ------------- |:-------------:| -----:|
+| domNode       | String | yes |
+| imageUrl       | String | yes |
+| color1       | Array [rgb] | yes |
+| color2       | Array [rgb] | yes |
